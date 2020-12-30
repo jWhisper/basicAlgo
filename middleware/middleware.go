10aaa0d52586ba4c33271eb6bcf2d4d610bb11c0
jwen <xjweny@163.com>
2020-12-30 15:52:40 +0800

    chanage code location

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,7 +1,5 @@
 package middleware
 
-import "fmt"
-
 type handler func() error
 
 type Middleware func(handler) handler
@@ -12,29 +10,5 @@ func Chain(first Middleware, others ...Middleware) Middleware {
 			last = others[i](last)
 		}
 		return first(last)
-	}
-}
-
-//testcase
-func handler1() error {
-	fmt.Println("handler1 print1:xxx")
-	return nil
-}
-
-func logger1() Middleware {
-	return func(h handler) handler {
-		return func() error {
-			fmt.Println("logger1 print:in logger1")
-			return h()
-		}
-	}
-}
-
-func logger2() Middleware {
-	return func(h handler) handler {
-		return func() error {
-			fmt.Println("logger2 print:in logger2")
-			return h()
-		}
 	}
 }
--- middleware/middleware_test.go
+++ middleware/middleware_test.go
@@ -1,8 +1,33 @@
 package middleware
 
 import (
+	"fmt"
 	"testing"
 )
+
+//testcase
+func handler1() error {
+	fmt.Println("handler1 print1:xxx")
+	return nil
+}
+
+func logger1() Middleware {
+	return func(h handler) handler {
+		return func() error {
+			fmt.Println("logger1 print:in logger1")
+			return h()
+		}
+	}
+}
+
+func logger2() Middleware {
+	return func(h handler) handler {
+		return func() error {
+			fmt.Println("logger2 print:in logger2")
+			return h()
+		}
+	}
+}
 
 func TestOneMiddleware(t *testing.T) {
 	log1 := logger1()
